21ec5f461c07f350a2b131fd719a72434b4fb733
Mohammad Prince <mprince2k16@gmail.com>
2020-05-04 03:50:02 +0600

    logdsad
    dsadsadasdasdsadasddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddas

--- _services/user.service.js
+++ _services/user.service.js
@@ -1,13 +1,7 @@
 import { authHeader } from "../_helpers";
 
 export const userService = {
-  login,
-  logout,
-  register,
-  getAll,
-  getById,
-  update,
-  delete: _delete
+  login
 };
 
 function login(email, password) {
@@ -28,59 +22,6 @@ function login(email, password) {
 
       return user;
     });
-}
-
-function logout() {
-  // remove user from local storage to log user out
-  localStorage.removeItem("user");
-}
-
-function register(user) {
-  const requestOptions = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(user)
-  };
-
-  return fetch(`/users/register`, requestOptions).then(handleResponse);
-}
-
-function getAll() {
-  const requestOptions = {
-    method: "GET",
-    headers: authHeader()
-  };
-
-  return fetch(`/users`, requestOptions).then(handleResponse);
-}
-
-function getById(id) {
-  const requestOptions = {
-    method: "GET",
-    headers: authHeader()
-  };
-
-  return fetch(`/users/${id}`, requestOptions).then(handleResponse);
-}
-
-function update(user) {
-  const requestOptions = {
-    method: "PUT",
-    headers: { ...authHeader(), "Content-Type": "application/json" },
-    body: JSON.stringify(user)
-  };
-
-  return fetch(`/users/${user.id}`, requestOptions).then(handleResponse);
-}
-
-// prefixed function name with underscore because delete is a reserved word in javascript
-function _delete(id) {
-  const requestOptions = {
-    method: "DELETE",
-    headers: authHeader()
-  };
-
-  return fetch(`/users/${id}`, requestOptions).then(handleResponse);
 }
 
 function handleResponse(response) {
